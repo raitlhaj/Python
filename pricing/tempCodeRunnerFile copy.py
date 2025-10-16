a4b18848bad79ae9311b78dc82481b94619bc12d
raitlhaj <99647177+raitlhaj@users.noreply.github.com>
2025-10-16 16:11:53 +0200

    Refacto

---
+++ pricing/tempCodeRunnerFile copy.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('ggplot')
+
+plt.rcParams['xtick.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 14
+plt.rcParams['figure.titlesize'] = 18
+plt.rcParams['figure.titleweight'] = 'medium'
+plt.rcParams['lines.linewidth'] = 2.5
